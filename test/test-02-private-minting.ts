2201d9274049c501e896910702ecc67d570ff838
zeeshanhanif <zeeshanhanif@gmail.com>
2022-08-30 23:46:34 +0500

    Added more test cases for private minting

--- test/test-02-private-minting.ts
+++ test/test-02-private-minting.ts
@@ -38,11 +38,12 @@ describe("Nusic Wave Forms NFT Deployed: Private Sales Started", function () {
       // Signature generations
       let messageHash = ethers.utils.solidityKeccak256(
         ["address","uint"],
-        [addr1.address,tokenCount]
+        [wallet.address,tokenCount]
       );
       let messageHashBinary = ethers.utils.arrayify(messageHash);
       let signature = await owner.signMessage(messageHashBinary);
       signatures.push(signature);
+      //console.log("signature "+(i+1)+" = ",signature);
     }
     //console.log(signatures);
   });
@@ -56,7 +57,7 @@ describe("Nusic Wave Forms NFT Deployed: Private Sales Started", function () {
     expect((await waveFormsNFT.connect(addr1).TREASURY_MAX())).to.be.equal(500);
     expect((await waveFormsNFT.connect(addr1).MAX_SUPPLY())).to.be.equal(10000);
     expect((await waveFormsNFT.connect(addr1).MINT_PER_TXT())).to.be.equal(50);
-    expect((await waveFormsNFT.connect(addr1).MINT_PER_ADDR())).to.be.equal(25);
+    expect((await waveFormsNFT.connect(addr1).MINT_PER_ADDR())).to.be.equal(100);
   });
 
   it("Price of each token is properly set", async function () {
@@ -97,7 +98,49 @@ describe("Nusic Wave Forms NFT Deployed: Private Sales Started", function () {
 
   it("Private Minting should be failed if privateSaleLive is false", async function () {
     const [owner,addr1] = await ethers.getSigners();
-    await expect((waveFormsNFT.connect(_accountList[0]).privateSaleMint(5,signatures[0]))).to.be.revertedWith("Ownable: caller is not the owner");
+    await expect((waveFormsNFT.connect(_accountList[0]).privateSaleMint(5,signatures[0]))).to.be.revertedWith("Private-Sale Closed");
   });
 
+  it("Make privateSaleLive to true successfully", async function () {
+    const [owner,addr1] = await ethers.getSigners();
+    expect((await waveFormsNFT.connect(owner).togglePrivateSaleLive())).to.be.ok;
+    expect((await waveFormsNFT.connect(addr1).privateSaleLive())).to.be.true;
+  });
+
+  it("Private Minting should be successfull for 20 token", async function () {
+    const [owner,addr1] = await ethers.getSigners();
+    //console.log("account 0 = ",_accountList[0]);
+    //console.log("signature 0 = ",signatures[0]);
+    expect((await waveFormsNFT.connect(_accountList[0]).privateSaleMint(20,signatures[0]))).to.be.ok;
+  });
+
+  it("privateSaleMinted = 20, preSaleMinted, publicSaleMinted, teamClaimMinted and treasuryMinted should zero", async function () {
+    const [owner,addr1] = await ethers.getSigners();
+    expect((await waveFormsNFT.connect(addr1).privateSaleMinted())).to.be.equal(20);
+    expect((await waveFormsNFT.connect(addr1).preSaleMinted())).to.be.equal(0);
+    expect((await waveFormsNFT.connect(addr1).publicSaleMinted())).to.be.equal(0);
+    expect((await waveFormsNFT.connect(addr1).teamClaimMinted())).to.be.equal(0);
+    expect((await waveFormsNFT.connect(addr1).treasuryMinted())).to.be.equal(0);
+  });
+
+  it("Private Minting should be successfull for 980 token", async function () {
+    const [owner,addr1] = await ethers.getSigners();
+    for(let i=1;i<_accountList.length;i++) {
+      expect((await waveFormsNFT.connect(_accountList[i]).privateSaleMint(20,signatures[i]))).to.be.ok;
+    }
+  });
+
+  it("privateSaleMinted = 1000, preSaleMinted, publicSaleMinted, teamClaimMinted and treasuryMinted should zero", async function () {
+    const [owner,addr1] = await ethers.getSigners();
+    expect((await waveFormsNFT.connect(addr1).privateSaleMinted())).to.be.equal(1000);
+    expect((await waveFormsNFT.connect(addr1).preSaleMinted())).to.be.equal(0);
+    expect((await waveFormsNFT.connect(addr1).publicSaleMinted())).to.be.equal(0);
+    expect((await waveFormsNFT.connect(addr1).teamClaimMinted())).to.be.equal(0);
+    expect((await waveFormsNFT.connect(addr1).treasuryMinted())).to.be.equal(0);
+  });
+
+  it("Private Minting should be failed because all allocated token minted", async function () {
+    const [owner,addr1] = await ethers.getSigners();
+    await expect((waveFormsNFT.connect(_accountList[0]).privateSaleMint(1,signatures[0]))).to.be.revertedWith("Private-Sale Quota will Exceed");
+  });
 });
